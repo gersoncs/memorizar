94d1d8b7d8bf0a3a5f79137de002941cd30e29f5
gersoncs <g_castasuns@hotmail.com>
2026-04-15 09:18:50 -0500

    inicial

---
+++ versomemo.js
@@ -0,0 +1,15 @@
+const versiculos = [
+    { semana: "Semana 1", cita: "Juan 15:9", texto: "Como el Padre me ha amado, así también yo os he amado; permaneced en mi amor." },
+    { semana: "Semana 2", cita: "Juan 17:3", texto: "Y ésta es la vida eterna: que te conozcan a ti, el único Dios verdadero, y a Jesucristo, a quien has enviado." },
+    { semana: "Semana 3", cita: "Lucas 14:11", texto: "Cualquiera que se enaltece será humillado, y el que se humilla será enaltecido." },
+    { semana: "Semana 4", cita: "Hebreos 4:12", texto: "La palabra de Dios es viva, eficaz y más cortante que toda espada de dos filos: penetra hasta partir el alma y el espíritu, las coyunturas y los tuétanos, y discierne los pensamientos y las intenciones del corazón." },
+    { semana: "Semana 5", cita: "Isaías 55:11", texto: "Así será mi palabra que sale de mi boca: no volverá a mí vacía, sino que hará lo que yo quiero y será prosperada en aquello para lo cual la envié." },
+    { semana: "Semana 6", cita: "Salmo 116:1-2", texto: "Amo a Jehová, pues ha oído mi voz y mis súplicas, porque ha inclinado a mí su oído; por tanto, lo invocaré en todos mis días." },
+    { semana: "Semana 7", cita: "Salmo 62:8", texto: "Pueblos, esperad en él en todo tiempo; derramad delante de él vuestro corazón; Dios es nuestro refugio." },
+    { semana: "Semana 8", cita: "Hebreos 11:1", texto: "Es, pues, la fe la certeza de lo que se espera, la convicción de lo que no se ve." },
+    { semana: "Semana 9", cita: "Salmo 119:93-94", texto: "Nunca jamás me olvidaré de tus mandamientos, porque con ellos me has vivificado. Tuyo soy yo, sálvame, porque he buscado tus mandamientos." },
+    { semana: "Semana 10", cita: "1 Juan 1:9", texto: "Si confesamos nuestros pecados, él es fiel y justo para perdonar nuestros pecados y limpiarnos de toda maldad." },
+    { semana: "Semana 11", cita: "Romanos 5:3-5", texto: "Y no sólo esto, sino que también nos gloriamos en las tribulaciones, sabiendo que la tribulación produce paciencia; y la paciencia, prueba; y la prueba, esperanza; y la esperanza no nos defrauda, porque el amor de Dios ha sido derramado en nuestros corazones por el Espíritu Santo que nos fue dado." },
+    { semana: "Semana 12", cita: "Isaías 50:4", texto: "Jehová el Señor me dio lengua de sabios, para saber hablar palabras al cansado; despertará mañana tras mañana, despertará mi oído para que escuche como los sabios." },
+    { semana: "Semana 13", cita: "1 Juan 3:2", texto: "Amados, ahora somos hijos de Dios y aún no se ha manifestado lo que hemos de ser; pero sabemos que cuando él se manifieste, seremos semejantes a él, porque lo veremos tal como él es." }
+];
